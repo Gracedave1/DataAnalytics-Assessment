23ef0a3cfe69d2f25c4c19a3f04312865d5b658c
Grace Onivehu <132106683+Gracedave1@users.noreply.github.com>
2025-05-19 13:20:01 +0100

    Update Assessment_Q2.sql

--- Assessment_Q2.sql
+++ Assessment_Q2.sql
@@ -6,7 +6,7 @@ TASK OVERVIEW:
 This query will evaluates customer engagement by:
 1. Calculating the number of successful transactions and the number of active months 
    (months with at least one transaction) over the past 12 months.
-2. Categorizing each customer into frequency bands based on their average transactions per month.
+2. Categorizing each customer into frequency category based on their average transactions per month.
 3. Summarizing how many customers fall into each frequency category and the average frequency.
 
 Categories:
@@ -36,24 +36,24 @@ WITH monthly_activity_stats AS (
 
 -- Step 2: Categorize customers by average monthly transaction frequency
 SELECT 
-    -- Assign a frequency band based on transaction rate per month
+    -- Assign a frequency category based on transaction rate per month
     CASE 
         WHEN total_transactions / active_months >= 10 THEN 'High Frequency'
         WHEN total_transactions / active_months >= 3 THEN 'Medium Frequency'
         ELSE 'Low Frequency'
     END AS frequency_category,
 
-    -- Count of customers in each frequency band
+    -- Count of customers in each frequency category
     COUNT(owner_id) AS customer_count,
 
-    -- Average monthly transaction frequency per band
+    -- Average monthly transaction frequency per category
     ROUND(AVG(total_transactions / active_months), 1) AS avg_transactions_per_month
 
 FROM monthly_activity_stats
 
 GROUP BY frequency_category
 
--- Ensure custom ordering of frequency bands
+-- Ensure custom ordering of frequency category
 ORDER BY CASE frequency_category
     WHEN 'High Frequency' THEN 1
     WHEN 'Medium Frequency' THEN 2
