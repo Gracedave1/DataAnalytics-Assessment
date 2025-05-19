cfdd660fac99163c9d1aea4aa7812e0493ff21bc
Grace Onivehu <132106683+Gracedave1@users.noreply.github.com>
2025-05-19 12:21:55 +0100

    Update Assessment_Q1.sql

--- Assessment_Q1.sql
+++ Assessment_Q1.sql
@@ -1,10 +1,10 @@
 -- Assessment_Q1.sql
 -- TASK: Retrieve customers with at least one funded savings plan and one funded investment plan
--- Sort their result by total deposits
+-- Sort their result by total deposits 
 
 /*
 TASK OVERVIEW: 
-This query returns a list of users who have:
+This query will returns a list of users who have:
 1. At least one regular savings plan (is_regular_savings = 1)
 2. At least one investment fund (is_a_fund = 1)
 3. Confirmed deposit transactions (transaction_status = 'success')
@@ -17,46 +17,34 @@ The expected output will includes:
 - Total deposits (converted from kobo to Naira)
 */
 
+
+-- SQL SCRIPT:
+-- Select the user's unique ID and Concatenate first and last names
 SELECT 
     u.id AS owner_id,
-    CONCAT(u.first_name, ' ', u.last_name) AS full_name,
+    CONCAT(u.first_name, ' ', u.last_name) AS name,
+    
+    -- Count distinct savings plans where is_regular_savings = 1 (i.e the user has a savings-type plan) 
+    -- and rename as savings_count
+    COUNT(CASE WHEN p.is_regular_savings = 1 THEN 1 END) AS savings_count,
 
-    -- This block of code will count distinct savings plans where is_regular_savings = 1
-    COUNT(DISTINCT CASE 
-        WHEN p.is_regular_savings = 1 THEN p.id 
-    END) AS savings_plan_count,
+    -- Count the number of funded investment plans where is_a_fund = 1 (i.e the user has an investment-type plan)
+    -- Sum up all confirmed deposits made by the user, divide by 100 to convert from kobo to Naira rounded to 2 decimal
+    COUNT(CASE WHEN p.is_a_fund = 1 THEN 1 END) AS investment_count,
+    ROUND(SUM(s.confirmed_amount) / 100.0, 2) AS total_deposits
+FROM savings_savingsaccount s
 
-    -- This will count distinct investment plans where is_a_fund = 1
-    COUNT(DISTINCT CASE 
-        WHEN p.is_a_fund = 1 THEN p.id 
-    END) AS investment_plan_count,
+    -- Joining the plans table to link each savings account to its plan & users table to retrieve user information
+JOIN plans_plan p ON s.plan_id = p.id
+JOIN users_customuser u ON s.owner_id = u.id
+    
+    -- filtering out only savings records that have confirmed deposits (inflow) & group results by user
+WHERE s.confirmed_amount > 0
+GROUP BY u.id, u.first_name, u.last_name 
 
-    -- A total Sum of confirmed deposits only for successful transactions
-    COALESCE(SUM(CASE 
-        WHEN s.transaction_status = 'success' THEN s.confirmed_amount 
-        ELSE 0 
-    END), 0) / 100 AS total_confirmed_deposits
-
-FROM users_customuser AS u
-
--- Joining user to their plans
-INNER JOIN plans_plan AS p 
-    ON u.id = p.owner_id
-
--- Joining plans to their corresponding savings account transactions
-LEFT JOIN savings_savingsaccount AS s 
-    ON p.id = s.plan_id
-
-WHERE 
-    p.is_deleted = 0       -- delete inactive plans
-    AND p.status_id = 1     -- Include only active plans
-
-GROUP BY 
-    u.id, u.first_name, u.last_name
-
+    -- Filter to include only users with at least one savings and one investment plan. 
+    -- Sort by total deposits in descending order
 HAVING 
-    savings_plan_count > 0 
-    AND investment_plan_count > 0
-
-ORDER BY 
-    total_confirmed_deposits DESC;
+    COUNT(CASE WHEN p.is_regular_savings = 1 THEN 1 END) > 0 AND
+    COUNT(CASE WHEN p.is_a_fund = 1 THEN 1 END) > 0
+ORDER BY total_deposits DESC;
